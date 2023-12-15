9278a1ff48d76f1c9b45ce8c07bb8b629e30c89d
Somin Lee <somin1214@gmail.com>
2023-12-15 17:01:31 -0600

    Time measurement included

--- BoyerMoore/src/BoyerMooreTest.java
+++ BoyerMoore/src/BoyerMooreTest.java
@@ -7,30 +7,55 @@ public class BoyerMooreTest {
 	
 	public static void main(String[] args) {
 		if (args.length == 0) { // Perform default behavior: print test cases
+			 
 			
 			// Test Case 1: Basic Test
+			long start1 = System.currentTimeMillis();
 	        testBoyerMoore("ABC", "ABCDAB", "Basic Test");
+	        long end1 = System.currentTimeMillis();      
+	        System.out.println("Elapsed Time in milli seconds: "+ (end1-start1));
 
 	        // Test Case 2: Pattern at the Beginning
+	        long start2 = System.currentTimeMillis();      
 	        testBoyerMoore("ABC", "ABCDEF", "Pattern at the Beginning");
-
+	        long end2 = System.currentTimeMillis();      
+	        System.out.println("Elapsed Time in milli seconds: "+ (end2-start2));
+	        
 	        // Test Case 3: Pattern at the End
+	        long start3 = System.currentTimeMillis();
 	        testBoyerMoore("ABC", "XYZABC", "Pattern at the End");
+	        long end3 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end3-start3));
 
 	        // Test Case 4: Multiple Occurrences
+	        long start4 = System.currentTimeMillis();
 	        testBoyerMoore("AB", "ABABAB", "Multiple Occurrences");
+	        long end4 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end4-start4));
 
 	        // Test Case 5: Pattern Not Found
+	        long start5 = System.currentTimeMillis();
 	        testBoyerMoore("ABC", "XYZ", "Pattern Not Found");
+	        long end5 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end5-start5));
 
 	        // Test Case 6: Empty String
+	        long start6 = System.currentTimeMillis();
 	        testBoyerMoore("ABC", "", "Empty String");
+	        long end6 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end6-start6));
 
 	        // Test Case 7: Empty Pattern
+	        long start7 = System.currentTimeMillis();
 	        testBoyerMoore("", "XYZ", "Empty Pattern");
+	        long end7 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end7-start7));
 	        
 	        // Test Case 8: Longer text
+	        long start8 = System.currentTimeMillis();
 	        testBoyerMoore("rab", "abacadabrabracabracadabrabrabracad", "Longer Text");
+	        long end8 = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end8-start8));
             
         } else {
 
@@ -40,7 +65,10 @@ public class BoyerMooreTest {
             if(isTxtFile(text)) {
             	readFileContents(pattern, text);
             } else {
-            	testBoyerMoore(pattern, text, "Boyer-Moore Result");
+                long start = System.currentTimeMillis();
+                testBoyerMoore(pattern, text, "Boyer-Moore Result");
+                long end = System.currentTimeMillis();
+    	        System.out.println("Elapsed Time in milli seconds: "+ (end-start));
             }
             
         }
@@ -59,7 +87,11 @@ public class BoyerMooreTest {
             Scanner scan = new Scanner(file);
             String content = scan.useDelimiter("\\A").next();
             scan.close();
+            
+            long start = System.currentTimeMillis();
             testBoyerMoore(pattern, content, "Boyer-Moore Result");
+            long end = System.currentTimeMillis();
+	        System.out.println("Elapsed Time in milli seconds: "+ (end-start));
             
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filename);
